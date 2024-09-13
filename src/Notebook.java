f36a04fab8c15a23e36014983acac910877d33f8
Harry Lawal <harrylawal74@gmail.com>
2024-09-13 14:26:53 +0100

    update Notebook.java -for loop -while loop -iterator objects complete

--- src/Notebook.java
+++ src/Notebook.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class Notebook
@@ -26,7 +27,7 @@ public class Notebook
         if(noteNumber < 0) {
             // This is not a valid note number, so do nothing.
         }
-        //-1 used so the input can be index 1
+        //-1 used so the input doesn't need to be index 0
         else if(noteNumber-1 < numberOfNotes()) {
             // This is a valid note number, so we can print it.
             System.out.println(notes.get(noteNumber-1));
@@ -36,12 +37,34 @@ public class Notebook
         }
     }
 
+    /* list notes using for loop
     public void listNotes()
     {
         //For each note in the notes collection, print the note
         //any string can replace 'note'
         for (String note : notes) {
             System.out.println(note);
+        }
+    }
+    */
+
+    /*list notes using while loop
+    public void listNotes()
+    {
+        int n = 0;
+        while(n < notes.size());
+        System.out.println(notes.get(n));
+        n++;
+    }
+    */
+
+
+    //list notes using iterator object
+    public void listNotes()
+    {
+        Iterator<String> iterator = notes.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
     }
 
